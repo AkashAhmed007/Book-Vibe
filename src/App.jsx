bed74b55d29958da5d3b3ab6744c2345d51fa0f1
Akash Ahmed <akashahmedlikhon.007@gmail.com>
2024-03-25 20:25:39 +0600

    navbar component created

--- src/App.jsx
+++ src/App.jsx
@@ -1,15 +1,12 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
 import './App.css'
+import NavBar from './components/NavBar'
 
 function App() {
-  const [count, setCount] = useState(0)
 
   return (
     <>
       
-      <h1>Welcome to my book review App</h1>
+      <NavBar></NavBar>
       
     </>
   )
---
+++ src/pages/ListedBooks.jsx
@@ -0,0 +1,5 @@
+export default function ListedBooks() {
+  return (
+    <div>ListedBooks</div>
+  )
+}
