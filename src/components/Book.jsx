a42393514cfd7e9e54ed1d2dcbd5141ddcbe3509
Akash Ahmed <akashahmedlikhon.007@gmail.com>
2024-03-26 01:48:06 +0600

    books section added

---
+++ src/components/Book.jsx
@@ -0,0 +1,26 @@
+import { Link } from "react-router-dom";
+
+export default function Book({book}) {
+    const{image,tags,bookName,author,category,rating}= book
+  return (
+    <div className="card bg-base-100 shadow-xl">
+        <figure className="px-10 pt-10">
+            <img src={image} alt="Shoes" className="rounded-xl w-1/2" />
+        </figure>
+        <div className="flex items-center gap-3 mt-4 ml-8">
+        {
+            tags.map(tag=>(<a className="text-green-600">#{tag}</a>))
+        }
+        </div>
+       
+        <div className="card-body">
+            <h2 className="card-title">{bookName}</h2>
+            <p>By:{author}</p>
+            <div className="flex justify-between">
+                <p>{category}</p>
+                <p>{rating}</p>
+            </div>
+        </div>
+    </div>
+  )
+}
---
+++ src/components/Books.jsx
@@ -0,0 +1,21 @@
+import { useLoaderData } from "react-router-dom";
+import Book from "./Book";
+
+export default function Books() {
+  const books = useLoaderData();
+  return (
+    <section className="dark:bg-gray-100 dark:text-gray-800 mt-5">
+	<div className="container max-w-6xl p-6 mx-auto space-y-6 sm:space-y-12">
+    <div className="space-y-2 text-center">
+			<h2 className="text-3xl font-bold">Books</h2>
+		</div>
+		<div className="grid justify-center grid-cols-1 gap-6 sm:grid-cols-2 lg:grid-cols-3">
+			{
+                books.map(book=><Book key={book.bookId} book={book}></Book>)
+            }
+			
+		</div>
+	</div>
+</section>
+  )
+}
